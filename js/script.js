5efda6d06bc33c4b9655bf1e93b7323dd885f363
Staffan Strömsholm <52479640+StaffanStromsholm@users.noreply.github.com>
2020-08-25 00:15:55 +0300

    added slideshow to aside

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
-var aside = document.getElementById("aside");
-var slideshowImages = ["123.JPG", "20171016_Ekroth_1.jpg", "profile_big5.jpg"](
-  function slide() {
-    setInterval(() => {
-      aside.style.backgroundImage = "url('/img/123.JPG')";
-    }, 2000);
-  }
+var aside1 = document.getElementById("aside1");
+var aside2 = document.getElementById("aside2");
+var aside3 = document.getElementById("aside3");
+
+aside1.style.backgroundImage = 'url("/img/123.JPG")';
+aside2.style.backgroundImage = 'url("/img/20171016_Ekroth_1.jpg")';
+aside3.style.backgroundImage = 'url("/img/profile_big5.jpg")';
+
+var asides = [aside1, aside2, aside3];
+
+var i = 0;
+
+// var slideshowImages = ["123.JPG", "20171016_Ekroth_1.jpg", "profile_big5.jpg"]
+// var i = 0;
+
+(function slide() {
+  setInterval(() => {
+    asides.forEach((item) => {
+      item.classList.add('hidden');
+    })
+    asides[i].classList.remove('hidden');
+    if (i < 2) {
+      i++
+    } else {
+      i = 0;
+    }
+  }, 8000);
+}
 )();
